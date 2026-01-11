d8a1738aec4bef9f75be03648ea452808812c35c
bonheur15 <rumanzibonheur@gmail.com>
2026-01-12 01:05:37 +0200

    FEAT: Customize session token generation
    - Import `crypto` module for secure token generation.
    - Add `session.generateSessionToken` to `NextAuth` configuration.
    - Prefix generated session tokens with "st_" for better identification.

--- auth.ts
+++ auth.ts
@@ -1,8 +1,15 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "./db"
- import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db";
+import Google from "next-auth/providers/google";
+import crypto from "crypto";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-   providers: [Google],
-})
+  session: {
+    generateSessionToken: () => {
+      const token = "st_" + crypto.randomBytes(64).toString("hex");
+      return token;
+    },
+  },
+  providers: [Google],
+});
